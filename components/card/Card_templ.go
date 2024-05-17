398de7a35296046ad5cc7ad194530e1790e568d1
Alma Tuck <alma.tuck@gmail.com>
2024-05-17 15:56:31 -0600

    work in progress

--- components/card/Card_templ.go
+++ components/card/Card_templ.go
@@ -118,6 +118,10 @@ func tpl(opts *Opts) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
+		templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
