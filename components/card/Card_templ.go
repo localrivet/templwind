faef3af48dccbd4b4607b8f5b922776d188e8e2c
Alma Tuck <alma.tuck@gmail.com>
2024-05-17 16:10:13 -0600

    work in progress

--- components/card/Card_templ.go
+++ components/card/Card_templ.go
@@ -27,7 +27,7 @@ func tpl(opts *Opts) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"p-4\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"p-4 h-full\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
